57f16e9b7ffd4cd2624ffc7c673af39d8a7758c3
Milan Jovanovic <milanjovanovic.km@gmail.com>
2023-03-01 00:54:18 +0100

    setup

---
+++ src/test/java/PolovniAutomobili.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PolovniAutomobili {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+    }
+
+    @AfterMethod
+    public void teardown() {
+
+    }
+
+
+    @Test
+    public void searchAutomobile() {
+
+    }
+}
